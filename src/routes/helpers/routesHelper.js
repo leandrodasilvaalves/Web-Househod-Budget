465fc74f888305c8e8e696f60ee8aee246a84119
Leandro Alves <leandro.silva.alves86@gmail.com>
2024-02-23 08:02:28 -0300

    Incluir configuracoes de listagem e data no ejs/env

--- src/routes/helpers/routesHelper.js
+++ src/routes/helpers/routesHelper.js
@@ -1,16 +1,23 @@
 require('dotenv').config();
 const path = require('path');
 
+const currentDate = new Date();
+const months = [...process.env.MONTH_NAMES?.split(',')];
+
 const options = {
     root: path.join(__dirname, 'views'),
     apiUrl: process.env.HOUSEHOLDBUDGET_URL,
     envName: process.env.ENV_NAME,
     theme: process.env.THEME,
+    year: currentDate.getFullYear(),
+    month: (currentDate.getMonth() + 1).toString().padStart(2, '0'),
+    monthName: months[currentDate.getMonth()],
+    pageSize: process.env.PAGE_SIZE || 20
 };
 
 const helper = {
     getOptions: () => options,
-    getView:(viewName)=> `pages/${viewName}`,
+    getView: (viewName) => `pages/${viewName}`,
 }
 
 module.exports = helper;
