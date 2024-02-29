0e02c11e243718eff55030faf2917f317e726410
Leandro Alves <leandro.silva.alves86@gmail.com>
2024-02-29 07:43:40 -0300

    Implementação inicial do orçamento

--- src/static/scripts/clients/monthlybudget.client.js
+++ src/static/scripts/clients/monthlybudget.client.js
@@ -1,7 +1,7 @@
 import httpclient from './http.client';
 
-const getOneMonthlyBudget = (year, month) =>
-    httpclient.get(`budget/${year}/${month}`);
+const getOneMonthlyBudget = async (year, month) =>
+    await httpclient.get(`budget/${year}/${month}`);
 
 export {
     getOneMonthlyBudget
--- src/static/scripts/services/monthlybudget/monthlybudget.getone.service.js
+++ src/static/scripts/services/monthlybudget/monthlybudget.getone.service.js
@@ -1,3 +1,58 @@
-export default () => {
-    
+import { getOneMonthlyBudget } from '@clients/monthlybudget.client';
+
+export default async () => {
+    if (page.isMonthlyBudget()) {
+        const { data, isSuccess } = await getOneMonthlyBudget(2024, "FEBRUARY");
+        if (isSuccess) {
+            page.bindCategories(data)
+        }
+    }
 };
+
+const page = {
+    accordion: document.getElementById('monthlybudget'),
+    isMonthlyBudget: () => window.location.pathname == '/monthlybudget',
+
+    bindCategories: ({categories}) => {
+        page.accordion.innerHTML = categories.map(category => {
+            const accordionId = `acc-${category.id}`;
+            return `
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button" type="button" 
+                            data-bs-toggle="collapse" data-bs-target="#${accordionId}" 
+                            aria-expanded="true" aria-controls="${accordionId}">
+                            ${category.name}
+                        </button>
+                    </h2>
+                    <div id="${accordionId}" class="accordion-collapse collapse show">
+                        <div class="container-fluid">
+                            <table class="table table-hover table-striped table-sm">               
+                                <tbody>
+                                    ${page.bindSubcategories(category.subcategories)}
+                                </tbody>
+                            </table>
+                        </div>               
+                    </div>
+                </div>`}).join('');
+    },
+    bindSubcategories: subcategories => {
+        const result = subcategories.map(subcategory => 
+            `<tr>
+                <th scope="row" data-subcategory=${subcategory.id}">${subcategory.name}</th>
+                <td>
+                    <div class="container text-center">
+                        <div class="row g-2">
+                            <div class="col-6">
+                                <small>${subcategory.total?.planned?.toFixed(2)}</small>
+                            </div>
+                            <div class="col-6">
+                                ${subcategory.total?.actual?.toFixed(2)}
+                            </div>                            
+                        </div>
+                    </div>
+                </td>               
+            </tr>`);
+        return result.join('');
+    }
+}
